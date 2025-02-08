6954730ecd9131365e602102d025dca8d8a6aa8a
ShreyP. <shreyprajapati13@gmail.com>
2025-02-08 17:18:36 +0530

    Enhance CORS middleware to support credentials and ensure proper frontend URL formatting

--- Backend/index.js
+++ Backend/index.js
@@ -10,9 +10,10 @@ app.use(express.json());
 // Allow requests from frontend
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL,
+    origin: `${process.env.FRONTEND_URL}`,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true
   })
 );
 
