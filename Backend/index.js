45cb9c1ed5d6a391c1993d513208a6d2318ab3b7
ShreyP. <shreyprajapati13@gmail.com>
2025-02-08 15:23:05 +0530

    Initialize backend with Express and dotenv configuration

---
+++ Backend/index.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import { configDotenv } from 'dotenv';
+
+configDotenv();
+
+const app = express();
+
+app.get('/',(req, res) => {
+  res.status(200).json({ message: 'Hello World' });
+})
+
+app.listen(process.env.PORT_NUMBER, () => {
+    console.log(`Server is running on port ${process.env.PORT_NUMBER}`);
+})
