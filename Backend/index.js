542c57a8d34e5c914570a00b1f2f2e107e737ce1
ShreyP. <shreyprajapati13@gmail.com>
2025-02-08 16:20:34 +0530

    Add CORS support and remove .gitignore file

--- Backend/index.js
+++ Backend/index.js
@@ -1,9 +1,13 @@
 import express from 'express';
 import { configDotenv } from 'dotenv';
+import cors from 'cors';
 
 configDotenv();
 
 const app = express();
+app.use(express.json());
+// Allow requests from localhost:3000 (or any other port)
+app.use(cors({ origin: "http://localhost:5173" })); // Adjust the port as needed
 
 app.get('/',(req, res) => {
   res.status(200).json({ message: 'Hello World' });
